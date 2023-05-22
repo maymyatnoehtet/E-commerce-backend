da60cfe4f77a521176d6e90e5dd3833dd70c2292
maymyatnoehtet <may.mn.htet@gmail.com>
2023-05-22 20:54:01 +1000

    sequelize in server.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,12 @@
+// Loads environment variables from the .env file
 require('dotenv').config();
 
+// Import the Sequelize library
 const Sequelize = require('sequelize');
 
+// Create the Sequelize connection object based on environment variables
 const sequelize = process.env.JAWSDB_URL
+// Connect to JawsDB MySQL in production environment
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
@@ -12,4 +16,5 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
+// Export the Sequelize connection object
 module.exports = sequelize;
